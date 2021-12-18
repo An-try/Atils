203d20012bdf5877beb5b133303918cad101d006
Andrey Mishchenko <andrey.anderey@gmail.com>
2021-12-18 14:33:11 +0200

    feat (extensions) : add Queue extensions

---
+++ Assets/Plugins/Atils/Runtime/Extensions/QueueExtensions.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atils.Runtime.Extensions
+{
+	public static class QueueExtensions
+	{
+		// TODO summary, tests
+		public static bool Remove<T>(this Queue<T> source, T item)
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (item == null) throw new ArgumentNullException(nameof(item));
+
+			if (source.Contains(item))
+			{
+				source = new Queue<T>(source.Where(x => !x.Equals(item)));
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
