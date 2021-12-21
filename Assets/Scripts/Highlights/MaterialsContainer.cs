ca009bfec442ed5252011e404deabea3b4bf0689
Andrey Mishchenko <andrey.anderey@gmail.com>
2021-12-21 17:12:23 +0200

    feat (highlights) : add logic

---
+++ Assets/Scripts/Highlights/MaterialsContainer.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class MaterialsContainer
+{
+	public Material[] Materials = default;
+
+	public MaterialsContainer(Material[] materials)
+	{
+		Materials = materials;
+	}
+}
