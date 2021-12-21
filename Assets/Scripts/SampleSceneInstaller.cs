5290a6b10202052609f386657b4b0d1992eccb92
Andrey Mishchenko <andrey.anderey@gmail.com>
2021-12-21 17:12:37 +0200

    test: highlights

--- Assets/Scripts/SampleSceneInstaller.cs
+++ Assets/Scripts/SampleSceneInstaller.cs
@@ -1,3 +1,4 @@
+using Atils.Runtime.Inputs;
 using Atils.Runtime.Pooling;
 using UnityEngine;
 using Zenject;
@@ -5,11 +6,24 @@ using Zenject;
 public class SampleSceneInstaller : MonoInstaller
 {
 	[SerializeField] private ObjectsPoolView _objectsPoolView = default;
+	[SerializeField] private MouseInputService _mouseInputService = default;
+	[SerializeField] private TouchInputService _touchInputService = default;
 
 	public override void InstallBindings()
 	{
 		// TODO save-load system
 
 		PoolingSystemBinder.Bind(Container, _objectsPoolView);
+
+		//if (JSLibraryProvider.IsMobile())
+		{
+			//Container.Bind<IInputService>().FromComponentInNewPrefab(_touchInputService).AsSingle().NonLazy();
+			//Debug.Log(this + " Binding touch input service.");
+		}
+		//else
+		{
+			Container.Bind<IInputService>().FromComponentInNewPrefab(_mouseInputService).AsSingle().NonLazy();
+			Debug.Log(this + " Binding mouse input service.");
+		}
 	}
 }
