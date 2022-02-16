0bde7ea854297065aab037610b24c6e07fd58527
Andrey Mishchenko <andrey.anderey@gmail.com>
2022-02-16 13:20:56 +0200

    feat (pooling tests) : setup test behaviour

--- Assets/Scripts/Tests/Pooling/Objects/SphereView.cs
+++ Assets/Scripts/Tests/Pooling/Objects/SphereView.cs
@@ -1,11 +1,41 @@
 using Atils.Runtime.Pooling;
+using UnityEngine;
 using Zenject;
 
 public class SphereView : PoolObject
 {
+	public Rigidbody Rigidbody;
+
 	public class Factory : PlaceholderFactory<IPoolObject>
 	{ }
 
+	Vector3 _direction = default;
+
+	public override void Initialize()
+	{
+		ReturnToPool(6);
+
+		float random = 10;
+
+		float x = Random.Range(-random, random);
+		float y = Random.Range(-random, random);
+		float z = Random.Range(0, random);
+
+		Vector3 randomScatter = new Vector3(x, y, z);
+		Vector3 direction = (Vector3.forward + randomScatter).normalized;
+
+		_direction = direction;
+	}
+
+	public override void UpdateObject(float timeStep)
+	{
+		base.UpdateObject(timeStep);
+
+		transform.Translate(_direction * 10 * timeStep, Space.World);
+	}
+
 	protected override void ResetObject()
-	{ }
+	{
+		//Rigidbody.velocity = new Vector3();
+	}
 }
