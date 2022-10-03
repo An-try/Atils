ca3554d5cc5c5dc8d3de9640b53c2590ee223576
Andrey Mishchenko <andrey.anderey@gmail.com>
2022-10-03 13:23:22 +0300

    update Enum extensions

--- Assets/Plugins/Atils/Scripts/Runtime/Extensions/EnumExtensions.cs
+++ Assets/Plugins/Atils/Scripts/Runtime/Extensions/EnumExtensions.cs
@@ -25,5 +25,26 @@ namespace Atils.Runtime.Extensions
 
 			return (T)Enum.Parse(sourceType, nextIndex.ToString());
 		}
+
+		public static T Previous<T>(this T source) where T : Enum
+		{
+			Type sourceType = source.GetType();
+			int currentIndex = (int)(object)source;
+
+			if (!Enum.IsDefined(sourceType, currentIndex))
+			{
+				throw new InvalidEnumArgumentException(nameof(source), currentIndex, sourceType);
+			}
+
+			int enumCount = Enum.GetNames(sourceType).Length;
+			int previousIndex = currentIndex - 1;
+
+			if (previousIndex < 0)
+			{
+				previousIndex = enumCount - 1;
+			}
+
+			return (T)Enum.Parse(sourceType, previousIndex.ToString());
+		}
 	}
 }
