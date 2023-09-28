fbf0175e91b1d6bd19f63027372769340c1e01a2
Andrey Mishchenko <andrey.anderey@gmail.com>
2023-09-29 01:29:36 +0300

    add bidon utils

---
+++ Assets/Plugins/BidOnGamesUtils/Runtime/Timers/CountdownTimer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEngine;
+
+namespace BidOnGamesUtils.Runtime.Timers
+{
+	public class CountdownTimer : ITimer, IDisposable
+	{
+		private int startSeconds = default;
+
+		private bool isRunning;
+		private float startTimeStamp = default;
+		private float pauseTimeStamp = default;
+
+		public int StartTime => startSeconds;
+
+		/// <summary>
+		/// Time left in seconds.
+		/// </summary>
+		public float RemainingTime => isRunning ?
+									  startSeconds - (Time.realtimeSinceStartup - startTimeStamp) :
+									  startSeconds - (pauseTimeStamp - startTimeStamp);
+
+		/// <summary>
+		/// Time elapsed since the start of the timer 
+		/// </summary>
+		public float ElapsedTime => startSeconds - RemainingTime;
+
+		public void Start(int startSeconds)
+		{
+			this.startSeconds = startSeconds;
+			isRunning = true;
+			startTimeStamp = Time.realtimeSinceStartup;
+		}
+
+		public void Stop()
+		{
+			Pause();
+		}
+
+		public void Pause()
+		{
+			if (isRunning)
+			{
+				isRunning = false;
+				// pauseTimeStamp must be set to correctly return the time after this timer has paused or stopped.
+				pauseTimeStamp = Time.realtimeSinceStartup;
+			}
+		}
+
+		public void UnPause()
+		{
+			if (!isRunning)
+			{
+				isRunning = true;
+				startTimeStamp += Time.realtimeSinceStartup - pauseTimeStamp;
+			}
+		}
+
+		/// <summary>
+		/// Decrease the time elapsed by this timer, but you cannot decrease the elapsed time less than 0.
+		/// </summary>
+		/// <param name="addSeconds"></param>
+		public void DecreaseTime(int addSeconds)
+		{
+			this.startTimeStamp += addSeconds;
+
+			if (this.startTimeStamp > Time.realtimeSinceStartup)
+			{
+				this.startTimeStamp = Time.realtimeSinceStartup;
+			}	
+		}
+
+		public void Dispose()
+		{
+			Stop();
+		}
+	}
+}
---
+++ Assets/Plugins/BidOnGamesUtils/Runtime/Timers/ITimer.cs
@@ -0,0 +1,15 @@
+﻿namespace BidOnGamesUtils.Runtime.Timers
+{
+	public interface ITimer
+	{
+		int StartTime { get; }
+		float RemainingTime { get; }
+		float ElapsedTime { get; }
+
+		void Start(int startSeconds);
+		void Stop();
+		void Pause();
+		void UnPause();
+		void DecreaseTime(int addSeconds);
+	}
+}
